eb7bbdeceb96ca814242d066112460bf2aaa83c1
GwonHeeJun <gwonhj9751@naver.com>
2020-05-13 23:04:57 +0900

    save : save project

--- app/src/main/java/com/example/icare/ConfigureActivity.java
+++ app/src/main/java/com/example/icare/ConfigureActivity.java
@@ -4,12 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.avast.android.dialogs.fragment.ListDialogFragment;
+import com.avast.android.dialogs.fragment.SimpleDialogFragment;
+
 
 public class ConfigureActivity extends AppCompatActivity {
 
     Button prev;
+    LinearLayout moniter, alram, birghtness;
+    ConfigureActivity c = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,55 @@ public class ConfigureActivity extends AppCompatActivity {
         setContentView(R.layout.activity_configure);
 
         prev = (Button)findViewById(R.id.prev);
+        moniter = (LinearLayout)findViewById(R.id.moniter);
+        alram = (LinearLayout)findViewById(R.id.alram);
+        birghtness = (LinearLayout)findViewById(R.id.birghtness);
+
+
+        moniter.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ListDialogFragment
+                        .createBuilder(c, getSupportFragmentManager())
+                        .setTitle("모니터링 거리지정")
+                        .setItems(new String[]{"15cm", "30cm", "45cm"})
+                        .setRequestCode(11)
+                        .setChoiceMode(AbsListView.CHOICE_MODE_SINGLE)
+                        .show();ㅈㅁㅁ
+                return false;
+            }
+        });
+
+        alram.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ListDialogFragment
+                        .createBuilder(c, getSupportFragmentManager())
+                        .setTitle("모니터링 종류 선택")
+                        .setItems(new String[]{"밝기줄이기", "알림띄우기"})
+                        .setRequestCode(11)
+                        .setChoiceMode(AbsListView.CHOICE_MODE_SINGLE)
+                        .show();
+
+
+                return false;
+            }
+        });
+
+        birghtness.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ListDialogFragment
+                        .createBuilder(c, getSupportFragmentManager())
+                        .setTitle("화면 밝기 지정")
+                        .setItems(new String[]{"50%", "40%", "30%", "20%", "10%", "0%"})
+                        .setRequestCode(11)
+                        .setChoiceMode(AbsListView.CHOICE_MODE_SINGLE)
+                        .show();
+                return false;
+            }
+        });
+
 
         prev.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,4 +86,6 @@ public class ConfigureActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
--- app/src/main/java/com/example/icare/RealMainActivity.java
+++ app/src/main/java/com/example/icare/RealMainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class RealMainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -27,6 +28,11 @@ public class RealMainActivity extends AppCompatActivity implements View.OnClickL
 
         switch (v.getId()){
             case R.id.start :
+                Intent intent = new Intent(Intent.ACTION_MAIN); //태스크의 첫 액티비티로 시작
+                intent.addCategory(Intent.CATEGORY_HOME);   //홈화면 표시
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); //새로운 태스크를 생성하여 그 태스크안에서 액티비티 추가
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "모니터링을 시작합니다.", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.setting :
                 startActivity(new Intent(getApplicationContext(), ConfigureActivity.class));
