c3c4a5d827d0e7d086207ac818357baa7845cab4
GwonHeeJun <gwonhj9751@naver.com>
2020-05-23 12:40:30 +0900

    modify : addtion email vaild

--- app/src/main/java/com/example/icare/MainActivity.java
+++ app/src/main/java/com/example/icare/MainActivity.java
@@ -7,14 +7,21 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -31,6 +38,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String sex = "";
     private String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
             + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -126,8 +134,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         editor.putString("sex", sex);
                         editor.commit();
 
-                        startActivity(new Intent(getApplicationContext(), SecureActivity.class));
-                        finish();
+                        db.collection("users").document(email.getText().toString()).get()
+                                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if(task.isSuccessful()) {
+                                            Toast.makeText(getApplicationContext(), "이미 존재하는 이메일입니다..", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                startActivity(new Intent(getApplicationContext(), SecureActivity.class));
+                                finish();
+                            }
+                        });
                     }
                     break;
                 } else {
